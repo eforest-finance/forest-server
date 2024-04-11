8e2739b421ac802ed627fdf272e41048d5f20c36
theodore <theodore@eforest.finance>
2024-04-11 18:08:22 +0800

    feat: add logic for nftcollection trade info init

--- src/NFTMarketServer.Domain/NFT/Index/IndexerNFTCollectionTrade.cs
+++ src/NFTMarketServer.Domain/NFT/Index/IndexerNFTCollectionTrade.cs
@@ -2,7 +2,7 @@ namespace NFTMarketServer.NFT.Index;
 
 public class IndexerNFTCollectionTrade : IndexerCommonResult<IndexerNFTCollectionTrade>
 {
-    public decimal VolumeTotal { get; set; } = -1;
+    public decimal VolumeTotal { get; set; } = 0;
     public decimal FloorPrice { get; set; } = -1;
-    public long SalesTotal { get; set; } = -1;
+    public long SalesTotal { get; set; } = 0;
 }
