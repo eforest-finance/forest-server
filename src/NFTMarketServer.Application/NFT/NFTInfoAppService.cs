fc24c6e304b91a0094954033e1cc375ff08956f1
kevin <kevin@eforest.finance>
2024-06-05 16:27:31 +0800

    remove unuse test code

--- src/NFTMarketServer.Application/NFT/NFTInfoAppService.cs
+++ src/NFTMarketServer.Application/NFT/NFTInfoAppService.cs
@@ -1072,11 +1072,6 @@ namespace NFTMarketServer.NFT
                 return;
             }
 
-            if (!fromNFTInfo.Symbol.Contains("SGR"))
-            {
-                return;
-            }
-
             var nftInfo = _objectMapper.Map<NFTInfoIndex, NFTInfoNewIndex>(fromNFTInfo);
 
             nftInfo.CountedFlag = FTHelper.IsGreaterThanEqualToOne(nftInfo.Supply, nftInfo.Decimals);
