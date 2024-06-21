a50eda05a4e007b1e38424ff2fbceaa9541e6f8a
theodore <theodore@eforest.finance>
2024-06-21 16:47:39 +0800

    feat: add NFTActivitySyncWorker

--- src/NFTMarketServer.Domain/NFT/Index/NFTActivityIndex.cs
+++ src/NFTMarketServer.Domain/NFT/Index/NFTActivityIndex.cs
@@ -2,6 +2,7 @@ using System;
 using AElf.Indexing.Elasticsearch;
 using Nest;
 using NFTMarketServer.Entities;
+using NFTMarketServer.NFT.Etos;
 
 namespace NFTMarketServer.NFT.Index;
 
@@ -11,11 +12,17 @@ public class NFTActivityIndex :  NFTMarketEntity<string>, IIndexBuild
     
     [Keyword] public string NftInfoId { get; set; }
     
+    public decimal Decimals { get; set; }
+    
     public NFTActivityType Type { get; set; }
     
     [Keyword] public string From { get; set; }
     
     [Keyword] public string To { get; set; }
+    
+    [Text(Index = false)] public string FullFromAddress { get; set; }
+    
+    [Text(Index = false)] public string FullToAddress { get; set; }
     
     public long Amount { get; set; }
     
@@ -24,4 +31,17 @@ public class NFTActivityIndex :  NFTMarketEntity<string>, IIndexBuild
     [Keyword] public string TransactionHash { get; set; }
     
     public DateTime Timestamp { get; set; }
+    
+    [Keyword] public string CollectionId { get; set; }
+    
+    [Keyword] public string NFTName { get; set; }
+    
+    public NFTType NFTType { get; set; }
+    
+    [Keyword] public string CollectionName { get; set; }
+
+    [Text(Index = false)] public string NFTImage { get; set; }
+
+    public bool ToNFTIssueFlag { get; set; }
+
 }
