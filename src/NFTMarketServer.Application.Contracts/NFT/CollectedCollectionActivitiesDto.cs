ac77706de532ad027e5179b468731ea4eb67a637
theodore <theodore@eforest.finance>
2024-06-24 21:56:05 +0800

    feat: add collected-collection-activities

--- src/NFTMarketServer.Application.Contracts/NFT/CollectedCollectionActivitiesDto.cs
+++ src/NFTMarketServer.Application.Contracts/NFT/CollectedCollectionActivitiesDto.cs
@@ -5,7 +5,5 @@ namespace NFTMarketServer.NFT
     public class CollectedCollectionActivitiesDto : NFTActivityDto
     {
         public string NFTName { get; set; }
-        public string NFTCollectionName { get; set; }
-        public string PreviewImage { get; set; }
     }
 }
