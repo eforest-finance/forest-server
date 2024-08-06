93fa813e04734f642aa3cbbffb6c1835bdc5247f
kevin <kevin@eforest.finance>
2024-08-06 14:53:00 +0800

    fix myhold list

--- src/NFTMarketServer.Application/NFT/Provider/NFTInfoNewSyncedProvider.cs
+++ src/NFTMarketServer.Application/NFT/Provider/NFTInfoNewSyncedProvider.cs
@@ -103,10 +103,16 @@ public class NFTInfoNewSyncedProvider : INFTInfoNewSyncedProvider, ISingletonDep
             mustQuery.Add(q => q.Terms(i => i.Field(f => f.IssueManagerSet).Terms(dto.IssueAddress)));
         }
 
-        if (!dto.SearchParam.IsNullOrEmpty())
+        if (!dto.SearchParam.IsNullOrEmpty() && !dto.fuzzySearchSwitch)
         {
             shouldQuery.Add(q => q.Term(i => i.Field(f => f.Symbol).Value(dto.SearchParam)));
             shouldQuery.Add(q => q.Term(i => i.Field(f => f.TokenName).Value(dto.SearchParam)));
+        }
+        
+        if (!dto.SearchParam.IsNullOrEmpty() && dto.fuzzySearchSwitch)
+        {
+            shouldQuery.Add(q => q.Wildcard(i => i.Field(f => f.FuzzySymbol).Value("*" + dto.SearchParam+ "*")));
+            shouldQuery.Add(q => q.Wildcard(i => i.Field(f => f.FuzzyTokenName).Value("*" + dto.SearchParam+ "*")));
         }
 
         if (!dto.ChainList.IsNullOrEmpty())
