a595329b1068450924ecfb7bae28d4bf132fdab8
kevin <kevin@eforest.finance>
2024-09-10 14:20:14 +0800

    add grain log

--- src/NFTMarketServer.Grains/Grain/NFTInfo/CreatePlatformNFTGrain.cs
+++ src/NFTMarketServer.Grains/Grain/NFTInfo/CreatePlatformNFTGrain.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using NFTMarketServer.Grains.Grain.Users;
 using NFTMarketServer.Users.Dto;
 using Orleans;
@@ -8,10 +10,14 @@ namespace NFTMarketServer.Grains.Grain.NFTInfo;
 public class CreatePlatformNFTGrain : Grain<CreatePlatformNFTState>, ICreatePlatformNFTGrain
 {
     private readonly IObjectMapper _objectMapper;
+    private readonly ILogger<CreatePlatformNFTGrain> _logger;
 
-    public CreatePlatformNFTGrain(IObjectMapper objectMapper)
+    public CreatePlatformNFTGrain(IObjectMapper objectMapper,
+        ILogger<CreatePlatformNFTGrain> logger)
     {
         _objectMapper = objectMapper;
+        _logger = logger;
+
     }
     
     public override async Task OnActivateAsync()
@@ -28,33 +34,51 @@ public class CreatePlatformNFTGrain : Grain<CreatePlatformNFTState>, ICreatePlat
 
     public async Task<GrainResultDto<CreatePlatformNFTGrainDto>> SaveCreatePlatformNFTAsync(CreatePlatformNFTGrainInput createPlatformNFTGrainInput)
     {
-        if (State == null || State.Address.IsNullOrEmpty())
+        try
         {
-            State = new CreatePlatformNFTState()
+            
+            if (State == null || State.Address.IsNullOrEmpty())
             {
-                Address = createPlatformNFTGrainInput.Address,
-                Count = 1
+                State = new CreatePlatformNFTState()
+                {
+                    Address = createPlatformNFTGrainInput.Address,
+                    Count = 1
+                };
+            }
+            else
+            {
+                State.Count += 1;
+            }
+
+            await WriteStateAsync();
+            return new GrainResultDto<CreatePlatformNFTGrainDto>()
+            {
+                Success = true,
+                Data = _objectMapper.Map<CreatePlatformNFTState, CreatePlatformNFTGrainDto>(State)
             };
         }
-        else
+        catch (Exception e)
         {
-            State.Count += 1;
+            _logger.LogError(e, "SaveCreatePlatformNFTAsync Exception input:{A} errMsg:{C}",JsonConvert.SerializeObject(createPlatformNFTGrainInput) ,e.Message);
+            throw e;
         }
 
-        await WriteStateAsync();
-        return new GrainResultDto<CreatePlatformNFTGrainDto>()
-        {
-            Success = true,
-            Data = _objectMapper.Map<CreatePlatformNFTState, CreatePlatformNFTGrainDto>(State)
-        };
     }
     
     public async Task<GrainResultDto<CreatePlatformNFTGrainDto>> GetCreatePlatformNFTAsync()
     {
-        return new GrainResultDto<CreatePlatformNFTGrainDto>()
+        try
         {
-            Success = true,
-            Data = _objectMapper.Map<CreatePlatformNFTState, CreatePlatformNFTGrainDto>(State)
-        };
+            return new GrainResultDto<CreatePlatformNFTGrainDto>()
+            {
+                Success = true,
+                Data = _objectMapper.Map<CreatePlatformNFTState, CreatePlatformNFTGrainDto>(State)
+            };
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "GetCreatePlatformNFTAsync Exception errMsg:{C}",e.Message);
+            throw e;
+        }
     }
 }
