99c65193373b6785e18a6ce43b8553b8cb260dad
kevin <kevin@eforest.finance>
2024-09-10 15:55:32 +0800

    fix review issues

--- src/NFTMarketServer.Application.Contracts/PlatformNFT/CreatePlatformNFTInput.cs
+++ src/NFTMarketServer.Application.Contracts/PlatformNFT/CreatePlatformNFTInput.cs
@@ -5,5 +5,6 @@ namespace NFTMarketServer.Platform;
 public class CreatePlatformNFTInput
 {
     [NotNull]public string NFTUrl { get; set; }
+    public string UrlHash { get; set; }
     [NotNull]public string NFTName { get; set; }
 }
