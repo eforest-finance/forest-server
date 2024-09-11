8dd946bf4a8424bbee85552c4fc6933215a9b4dc
kevin <kevin@eforest.finance>
2024-09-11 21:26:39 +0800

    code done

--- src/NFTMarketServer.Application.Contracts/PlatformNFT/IPlatformNFTAppService.cs
+++ src/NFTMarketServer.Application.Contracts/PlatformNFT/IPlatformNFTAppService.cs
@@ -4,8 +4,6 @@ namespace NFTMarketServer.Platform
 {
     public interface IPlatformNFTAppService
     {
-        Task<CreatePlatformNFTOutput> CreatePlatformNFTV1Async(CreatePlatformNFTInput input);
-        
         Task<CreatePlatformNFTOutput> CreatePlatformNFTAsync(CreatePlatformNFTInput input);
         Task<CreatePlatformNFTRecordInfo> GetPlatformNFTInfoAsync(string address);
 
