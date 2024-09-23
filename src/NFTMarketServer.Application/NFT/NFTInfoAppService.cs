85e214ddd55ac4278abe1e8fc007a4ba19541297
kevin <kevin@eforest.finance>
2024-09-23 14:05:52 +0800

    add log

--- src/NFTMarketServer.Application/NFT/NFTInfoAppService.cs
+++ src/NFTMarketServer.Application/NFT/NFTInfoAppService.cs
@@ -2196,9 +2196,15 @@ namespace NFTMarketServer.NFT
             {
                 var seedResult = await _seedSymbolSyncedProvider.GetSeedBriefInfosAsync(getCompositeNFTInfosInput);
                 //to get max offers
+                _logger.LogInformation("GetMyCreatedNFTInfosAsyncV2 Seed seedResult:{seedResult}", JsonConvert.SerializeObject(seedResult));
+
                 var maxOfferDict = await GetMaxOfferInfosAsync(seedResult.Item2.Select(info => info.Id).ToList());
+                _logger.LogInformation("GetMyCreatedNFTInfosAsyncV2 Seed maxOfferDict:{maxOfferDict}", JsonConvert.SerializeObject(maxOfferDict));
+
                 var minListDict = await GetMyMinListInfosAsync(seedResult.Item2.Select(info => info.Symbol).ToList(),
                     input.Address, input.ChainList.FirstOrDefault());
+                _logger.LogInformation("GetMyCreatedNFTInfosAsyncV2 Seed minListDict:{minListDict}", JsonConvert.SerializeObject(minListDict));
+
                 var queryUserBalanceIndexInput = new QueryUserBalanceIndexInput()
                 {
                     Address = input.Address,
@@ -2209,12 +2215,15 @@ namespace NFTMarketServer.NFT
                 };
                 var userBalanceList =
                     await _userBalanceIndexProvider.GetValidUserBalanceInfosAsync(queryUserBalanceIndexInput);
+                _logger.LogInformation("GetMyCreatedNFTInfosAsyncV2 Seed userBalanceList:{userBalanceList}", JsonConvert.SerializeObject(userBalanceList));
 
                 var accountDtoDict =
                     await _userAppService.GetAccountsAsync(seedResult.Item2.Select(info => info.RealOwner).ToList());
 
                 seedPageResult = PageSeedBriefInfoIndexDto(input, seedResult.Item2, maxOfferDict, accountDtoDict,
                     minListDict, userBalanceList);
+                _logger.LogInformation("GetMyCreatedNFTInfosAsyncV2 Seed seedPageResult:{seedPageResult}", JsonConvert.SerializeObject(seedPageResult));
+
             }
             catch (Exception e)
             {
