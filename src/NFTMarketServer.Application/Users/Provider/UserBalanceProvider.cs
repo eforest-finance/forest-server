9f92f6c22c90eb11612cf0104ea337421139bf21
kevin <kevin@eforest.finance>
2024-09-23 15:08:09 +0800

    add log

--- src/NFTMarketServer.Application/Users/Provider/UserBalanceProvider.cs
+++ src/NFTMarketServer.Application/Users/Provider/UserBalanceProvider.cs
@@ -171,7 +171,7 @@ public class UserBalanceProvider : IUserBalanceProvider, ISingletonDependency
             {
                 return userBalanceList;
             }
-            _logger.LogDebug("GetValidUserBalanceInfosAsync for debug query userBalance count:{A} size:{} input:{C}", result.Item1, result.Item2.Count, JsonConvert.SerializeObject(queryUserBalanceIndexInput));
+            _logger.LogDebug("GetValidUserBalanceInfosAsync for debug query userBalance count:{A} size:{size} input:{C}", result.Item1, result.Item2.Count, JsonConvert.SerializeObject(queryUserBalanceIndexInput));
 
             if (queryCount == CommonConstant.IntOne)
             {
