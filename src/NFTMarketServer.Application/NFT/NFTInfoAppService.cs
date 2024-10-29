9ccbc24547b2bc500313b9c36f23caaee08d9c12
theodore <theodore@eforest.finance>
2024-10-29 20:06:04 +0800

    feat: modify nft deal info

--- src/NFTMarketServer.Application/NFT/NFTInfoAppService.cs
+++ src/NFTMarketServer.Application/NFT/NFTInfoAppService.cs
@@ -1863,6 +1863,7 @@ namespace NFTMarketServer.NFT
                 MaxResultCount = 1
             };
             var dealInfos = await _nftDealInfoProvider.GetDealInfosAsync(dto);
+            if (dealInfos == null) return null;
             return dealInfos.IndexerNftDealList.FirstOrDefault();
         }
 
