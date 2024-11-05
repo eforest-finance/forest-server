ae384dcd4a078872e8b7938afc8ff45464b1d768
theodore <theodore@eforest.finance>
2024-11-05 13:58:43 +0800

    feat: add log

--- src/NFTMarketServer.Application/NFT/NFTCollectionAppService.cs
+++ src/NFTMarketServer.Application/NFT/NFTCollectionAppService.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
@@ -473,6 +474,8 @@ namespace NFTMarketServer.NFT
 
         public async Task<PagedResultDto<SearchNFTCollectionsDto>> GetMyHoldNFTCollectionsAsync(GetMyHoldNFTCollectionsInput input)
         {
+            var stopwatch = Stopwatch.StartNew();
+            Logger.LogDebug("GetMyHoldNFTCollectionsAsync 0");
             var queryUserBalanceIndexInput = new QueryUserBalanceIndexInput()
             {
                 Address = input.Address,
@@ -481,6 +484,8 @@ namespace NFTMarketServer.NFT
                 SkipCount = CommonConstant.IntZero
             };
             var userBalanceList = await _userBalanceProvider.GetValidUserBalanceInfosAsync(queryUserBalanceIndexInput);
+            var spend1 = stopwatch.ElapsedMilliseconds;
+            Logger.LogDebug("GetMyHoldNFTCollectionsAsync 1 {A}",spend1);
             if (userBalanceList.IsNullOrEmpty())
             {
                 return new PagedResultDto<SearchNFTCollectionsDto>()
@@ -495,7 +500,8 @@ namespace NFTMarketServer.NFT
 
             var collectionDictionary = await _nftCollectionProvider.GetNFTCollectionIndexByIdsAsync(collectionIds);
             _logger.LogDebug("GetMyHoldNFTCollectionsAsync for debug query userBalance collectionDictionary:{A}", JsonConvert.SerializeObject(collectionDictionary));
-
+            var spend2 = stopwatch.ElapsedMilliseconds;
+            Logger.LogDebug("GetMyHoldNFTCollectionsAsync 2 {A} {B}",spend2,spend2-spend1);
             if (collectionDictionary == null || collectionDictionary.Count == CommonConstant.IntZero)
             {
                 return new PagedResultDto<SearchNFTCollectionsDto>()
@@ -510,7 +516,8 @@ namespace NFTMarketServer.NFT
                 CollectionIdList = collectionIds
             });
             var extensionList = tuple.Item2;
-
+            var spend3 = stopwatch.ElapsedMilliseconds;
+            Logger.LogDebug("GetMyHoldNFTCollectionsAsync 3 {A} {B}",spend3,spend3-spend2);
             List<SearchNFTCollectionsDto> nftCollectionIndexDtos = new List<SearchNFTCollectionsDto>();
             foreach (var indexerNFTCollection in collectionDictionary.Values)
             {
