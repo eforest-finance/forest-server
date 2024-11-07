ad4a5ccf33b11a065c0b91d8faaf31104c9191a1
theodore <theodore@eforest.finance>
2024-11-07 09:44:59 +0800

    feat: modify blockHeight logic

--- src/NFTMarketServer.Application/ScheduleSyncDataService.cs
+++ src/NFTMarketServer.Application/ScheduleSyncDataService.cs
@@ -46,7 +46,17 @@ public abstract class ScheduleSyncDataService : IScheduleSyncDataService
                 _logger.LogInformation(
                     "Start deal data for businessType: {businessQueryChainType} chainId: {chainId} lastEndHeight: {lastEndHeight} newIndexHeight: {newIndexHeight}",
                     businessQueryChainType, chainId, lastEndHeight, newIndexHeight);
+                var preLastEndHeight = lastEndHeight;
                 var blockHeight = await SyncIndexerRecordsAsync(chainId, lastEndHeight, newIndexHeight);
+                if (preLastEndHeight == lastEndHeight && lastEndHeight < newIndexHeight)
+                {
+                    var realBlockHeight = blockHeight;
+                    blockHeight += 1;
+                    _logger.LogInformation(
+                        "blockHeight keep same then change for businessType: {businessQueryChainType} chainId: {chainId} " +
+                        "preLastEndHeight:{A} realBlockHeight: {B} BlockHeight:{C}",
+                        businessQueryChainType, chainId, preLastEndHeight, realBlockHeight, blockHeight);
+                }
 
                 if (blockHeight > 0)
                 {
