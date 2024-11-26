bbd794685c7fd755ef47ae921109d7401f1e077b
suerlych <19871208@lhcyf>
2024-11-26 11:40:14 +0800

    init collection map

--- src/NFTMarketServer.Silo/MongoDB/ForestMongoCollectionConstants.cs
+++ src/NFTMarketServer.Silo/MongoDB/ForestMongoCollectionConstants.cs
@@ -8,19 +8,25 @@ public class ForestMongoCollectionConstants
         {"NFTMarketServer.Grains.State.Dealer.ContractInvokeState", "ContractInvokeGrain"},
         {"NFTMarketServer.Grains.State.Icon.SymbolIconGrainState", "SymbolIconGrain"},
         {"NFTMarketServer.Grains.State.Inscription.InscriptionAmountState", "InscriptionAmountGrain"},
-        {"NFTMarketServer.Grains.State.Inscription.InscriptionInscribeState", "InscriptionInscribeGrain"},//guid
+        //{"NFTMarketServer.Grains.State.Inscription.InscriptionInscribeState", "InscriptionInscribeGrain"},//guid 1
         {"NFTMarketServer.Grains.State.Inscription.InscriptionItemCrossChainState", "InscriptionItemCrossChainGrain"},
         {"NFTMarketServer.Grains.Grain.Users.CreatePlatformNFTState", "CreatePlatformNFTGrain"},
         {"NFTMarketServer.Grains.State.NFTInfo.NftCollectionExtensionState", "NftCollectionExtensionGrain"},
         {"NFTMarketServer.Grains.State.NFTInfo.NftInfoExtensionState", "NftInfoExtensionGrain"},
         {"NFTMarketServer.Grains.Grain.Users.PlatformNFTTokenIdState", "PlatformNFTTokenIdGrain"},
-        {"NFTMarketServer.Grains.State.Order.NFTOrderState", "NFTOrderGrain"},
         {"NFTMarketServer.Grains.State.Synchronize.SynchronizeState", "SynchronizeTxJobGrain"},
         {"NFTMarketServer.Grains.State.NFTInfo.TreeUserActivityRecordState", "TreeUserActivityRecordGrain"},
         {"NFTMarketServer.Grains.State.NFTInfo.TreeUserInfoState", "TreeUserInfoGrain"},
         {"NFTMarketServer.Grains.State.NFTInfo.TreeUserPointsDetailState", "TreeUserPointsDetailGrain"},
-        {"NFTMarketServer.Grains.State.Users.UserState", "UserGrain"},//guid
-        {"NFTMarketServer.Grains.State.Verify.OwnerShipVerifyOrderState", "OwnerShipVerifyOrderGrain"}
-    };
+        {"NFTMarketServer.Grains.State.Order.NFTOrderState", "NFTOrderGrain"},
 
+        //not find code
+        //{"", "SynchronizeTransactionJobGrain"},
+        //{"", "VersionStoreGrain"},//guid 1
+
+        //discard
+        {"NFTMarketServer.Grains.State.Verify.OwnerShipVerifyOrderState", "OwnerShipVerifyOrderGrain"},
+        {"NFTMarketServer.Grains.State.Order.NFTOrderState", "NFTOrderGrain"},
+
+    };
 }
--- src/NFTMarketServer.Silo/MongoDB/ForestMongoGrainIdConstants.cs
+++ src/NFTMarketServer.Silo/MongoDB/ForestMongoGrainIdConstants.cs
@@ -8,19 +8,23 @@ public class ForestMongoGrainIdConstants
         {"ContractInvoke","GrainReference=a29db13d603fa7b16c171f67e052ae9403ffffffc9d19f6b"},
         {"SymbolIcon","GrainReference=0000000000000000000000000000000006ffffffb5a4dc5e"},
         {"InscriptionAmount","GrainReference=000000000000000000000000000000000600000068d4baf5"},
-        //{"InscriptionInscribe","GrainReference=449fda8bab1c2a824c896d2425a6348203ffffffc15159ed"},//guid
         {"InscriptionItemCrossChain","GrainReference=0000000000000000000000000000000006ffffffc31194a9"},
         {"CreatePlatformNFT","GrainReference=0000000000000000000000000000000006ffffffcc77546a"},
         {"NftCollectionExtension","GrainReference=00000000000000000000000000000000060000000c3ae72f"},
         {"NftInfoExtension","GrainReference=0000000000000000000000000000000006ffffffbb5fab22"},
         {"PlatformNFTTokenId","GrainReference=0000000000000000000000000000000006ffffff80495c7a"},
-       // {"NFTOrder",""},
         {"SynchronizeTxJob","GrainReference=0000000000000000000000000000000006ffffffad61444b"},
         {"TreeUserActivityRecord","GrainReference=0000000000000000000000000000000006ffffffb6474e1a"},
         {"TreeUserInfo","GrainReference=00000000000000000000000000000000060000001521c856"},
         {"TreeUserPointsDetail","GrainReference=0000000000000000000000000000000006ffffff8d2c1ace"},
-       // {"User",""},//guid
-        {"OwnerShipVerifyOrder",""}
+       
+        // {"User",""},//guid
+        // {"InscriptionInscribe","GrainReference=449fda8bab1c2a824c896d2425a6348203ffffffc15159ed"},//guid
+        
+        //discard
+        //{"OwnerShipVerifyOrder",""}
+        //{"NFTOrder",""},
+
     };
 
 }
