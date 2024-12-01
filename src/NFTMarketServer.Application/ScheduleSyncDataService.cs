c0100a9644409c46104b230d74ccc414cffa5a65
Jecket1 <134037206+Jecket1@users.noreply.github.com>
2024-12-01 10:21:03 +0800

    Inscription AeFinder V2 (#433)
    * feat: modify NFTMarketServerDealerModule
    * feat: add log for ContractInvokeHandler
    * Inscription AeFinder V2 (#425)
    * feat: modify collection image
    * feat: modify syncstate logic

--- src/NFTMarketServer.Application/ScheduleSyncDataService.cs
+++ src/NFTMarketServer.Application/ScheduleSyncDataService.cs
@@ -43,7 +43,7 @@ public abstract class ScheduleSyncDataService : IScheduleSyncDataService
         }
 
         var lastEndHeight = await _graphQlProvider.GetLastEndHeightAsync(chainId, businessQueryChainType);
-        var newIndexHeight = await _graphQlProvider.GetIndexBlockHeightAsync(chainId);
+        var newIndexHeight = await _graphQlProvider.GetIndexBlockHeightAsync(chainId, businessQueryChainType);
         _logger.LogInformation(
             "Start deal data for businessType: {businessQueryChainType} chainId: {chainId} lastEndHeight: {lastEndHeight} newIndexHeight: {newIndexHeight}",
             businessQueryChainType, chainId, lastEndHeight, newIndexHeight);
